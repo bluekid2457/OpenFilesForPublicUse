32f6357a75dc034b4a1aa7de6f0454f960ee915e
Bluekid <bluekid2457@gmail.com>
2023-07-14 18:29:50 +0530

    test js local host added

--- test.js
+++ test.js
@@ -36,7 +36,12 @@ const generateResponse = (chatElement) => {
     company_name = (window.location != window.parent.location)
     ? document.referrer
     : document.location.href;
-    // company_name= company_name.split("/")[2]
+    if (company_name.includes("127.0.0.1")){
+        company = "localhost"
+    }
+    else{
+        company_name= company_name.split("/")[2]
+    }
     console.log(company_name)
     fetch("https://d7quvtbtqi.execute-api.us-west-2.amazonaws.com/default/testFunc1?company="+company_name +"&uinput="+userMessage).then(res => res.text()).then(data => {
         // console.log("Problem 0")    
