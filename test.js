76deb9a5b60d96d097e8b735ef14d37ab11259dd
Bluekid <bluekid2457@gmail.com>
2023-07-27 16:01:17 +0530

    Update test.js

--- test.js
+++ test.js
@@ -36,7 +36,7 @@ const generateResponse = (chatElement) => {
     company_name = (window.location != window.parent.location)
     ? document.referrer
     : document.location.href;
-    if (company_name.includes("127.0.0.1")){
+    if (company_name.includes("127.0.0.1") || company_name.includes("localhost")){
         company_name = "localhost"
     }
     else{
