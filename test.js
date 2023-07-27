15fe00329c27f4a09e804a71a0661b40188646e7
Bluekid <bluekid2457@gmail.com>
2023-07-27 21:40:49 +0530

    Update test.js

--- test.js
+++ test.js
@@ -87,7 +87,7 @@ const handleChat = () => {
     
     setTimeout(() => {
         // Display "Thinking..." message while waiting for the response
-        const incomingChatLi = createChatLi("Thinking...", "incoming");
+        const incomingChatLi = createChatLi("Analyzing...", "incoming");
         chatbox.appendChild(incomingChatLi);
         chatbox.scrollTo(0, chatbox.scrollHeight);
         generateResponse(incomingChatLi);
